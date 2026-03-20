8ef6275d26139c4fa6e9b6d66125f57db7c5c100
jhojahn <jhojahnb@gmail.com>
2026-03-20 13:39:13 +0100

    fix:arregl0 de c0dig0

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 import os
 
+
 app = FastAPI()
 
+
 APP_NAME = os.getenv("APP_NAME", "gestion-proyectos-tareas-backend")
+
 
 @app.get("/")
 def root():
@@ -12,6 +15,8 @@ def root():
         "app_name": APP_NAME
     }
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,13 +3,17 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     data = response.json()
     assert data["message"] == "API funcionando"
 
+
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
+
