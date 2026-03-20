8ed4cf798d10b80060fcade0c7a0bd882c19865d
jhojahn <jhojahnb@gmail.com>
2026-03-20 13:30:53 +0100

    test: new tests

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+APP_NAME = os.getenv("APP_NAME", "gestion-proyectos-tareas-backend")
+
+@app.get("/")
+def root():
+    return {
+        "message": "API funcionando",
+        "app_name": APP_NAME
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
