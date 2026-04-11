265bf3ea43684f3b9357081aa373561832269395
jhojahn <jhojahnb@gmail.com>
2026-04-11 17:32:24 +0200

    Add: User model

---
+++ app/models/user.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field, Relationship
+
+class Usuario(SQLModel, table=True):
+    id_usuario: int | None = Field(default=None, primary_key=True)
+    nombre: str
+    email: str
+    password: str
+    id_rol: int = Field(foreign_key="rol.id_rol")
+
+    rol: "Rol" = Relationship(back_populates="usuarios")
+    tareas_asignadas: list["Tarea"] = Relationship(back_populates="usuario_asignado")
+    comentarios: list["Comentario"] = Relationship(back_populates="usuario")
